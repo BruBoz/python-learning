a9d3a4a8fd73b3400d41744448fd96e2891e98d5
BruBoz <bozek.bruno@gmail.com>
2026-08-01 20:07:39 +0200

    Added planet quiz

--- quiz.py
+++ quiz.py
@@ -1,18 +1,18 @@
 score = 0
 answer = input("Which planet is the eighth planet from the Sun?")
-if answer.lower() == "Neptune":
+if answer.lower() == " neptune":
     print("Correct! ✅")
     score += 1
 else: 
     print("Incorrect! ❌ The correct answer is Neptune.")
-answer = input("Which planet in the Solar System is the biggest?")
-if answer.lower() == "Jupiter":
+answer = input("Which planet has the most volcanoes?")
+if answer.lower() == " venus":
     print("Correct! ✅")
     score += 1
 else:
     print ("Incorrect! ❌ The correct answer is Jupiter")
 answer = input("Which planet has the second biggest amount of moons in the Solar System?")
-if answer.lower() == "Saturn":
+if answer.lower() == " jupiter":
     print("Correct! ✅")
     score += 1
 else:
