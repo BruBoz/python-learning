409a431df91ffd79f2f0fad292a6d0de1a1c810a
BruBoz <bozek.bruno@gmail.com>
2026-08-03 14:12:21 +0200

    Added def_learning

---
+++ functions_practice.py
@@ -0,0 +1,24 @@
+def welcome():
+    print("Hello!")
+    print("Welcome to my game!")
+
+def ask_name():
+    name = input("What's your name?")
+    print(f"Hello, {name}!")
+
+def ask_age():
+    age = input("How old are you?")
+    print(f"Thanks! Now I know that you're {age} years old.")
+
+def goodbye():
+    print("Thanks for playing!")
+    print("See you next time!")
+
+welcome()
+
+ask_name()
+
+ask_age()
+
+goodbye()
+
